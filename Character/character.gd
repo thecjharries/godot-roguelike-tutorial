3ffa24546717793faeca0f09efb813721a204a33
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-02 13:57:06 -0600

    Add onready animation

--- Character/character.gd
+++ Character/character.gd
@@ -7,6 +7,8 @@ const FRICTION: float = 0.15
 @export var acceleration: int = 40
 @export var max_speed: int = 100
 
+@onready var animated_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
+
 var move_direction: Vector2 = Vector2.ZERO
 
 func _physics_process(_delta: float) -> void:
