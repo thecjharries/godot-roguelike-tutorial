86c84d4acb29e0b4265727821d4dcc58b15a7c08
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-02 13:45:47 -0600

    Add movement setup for the Character class

--- Character/character.gd
+++ Character/character.gd
@@ -1,11 +1,19 @@
+@icon("res://Art/v1.1 dungeon crawler 16X16 pixel pack/heroes/knight/knight_idle_anim_f0.png")
 extends CharacterBody2D
+class_name Character
 
+const FRICTION: float = 0.15
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var acceleration: int = 40
+@export var max_speed: int = 100
 
+var move_direction: Vector2 = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+
+func move() -> void:
+	move_direction = move_direction.normalized()
+	velocity += move_direction * acceleration
+	velocity = velocity.limit_length(max_speed)
