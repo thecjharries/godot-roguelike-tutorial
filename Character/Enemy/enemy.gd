e51c897d1537ad89b091c9820ca5492928f61d32
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-07 18:58:18 -0600

    Implement Enemy script

--- Character/Enemy/enemy.gd
+++ Character/Enemy/enemy.gd
@@ -1,2 +1,29 @@
 @icon("res://Art/v1.1 dungeon crawler 16X16 pixel pack/props_itens/prisoner.png")
-extends CharacterBody2D
+extends Character
+class_name Enemy
+
+@onready var player: CharacterBody2D = get_tree().current_scene.get_node("Player")
+@onready var path_timer: Timer = get_node("PathTimer")
+@onready var navigation_agent: NavigationAgent2D = get_node("NavigationAgent2D")
+
+func chase() -> void:
+	if not navigation_agent.is_target_reached():
+		var vector_to_next_point: Vector2 = navigation_agent.get_next_path_position() - global_position
+		move_direction = vector_to_next_point
+
+		if vector_to_next_point.x > 0 and animated_sprite.flip_h:
+			animated_sprite.flip_h = false
+		elif vector_to_next_point.x < 0 and not animated_sprite.flip_h:
+			animated_sprite.flip_h = true
+
+
+func _on_PathTimer_timeout() -> void:
+	if is_instance_valid(player):
+		_get_path_to_player()
+	else:
+		path_timer.stop()
+		move_direction = Vector2.ZERO
+
+
+func _get_path_to_player() -> void:
+	navigation_agent.target_position = player.position
