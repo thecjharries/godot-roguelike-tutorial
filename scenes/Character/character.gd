9b58a0d97600617e8cf2c2661243203bd4dfbdac
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-11 18:21:45 -0600

    Wire up child nodes

--- scenes/Character/character.gd
+++ scenes/Character/character.gd
@@ -9,6 +9,9 @@ export(int) var max_speed: int = 100
 var move_direction: Vector2 = Vector2.ZERO
 var velocity: Vector2 = Vector2.ZERO
 
+onready var state_machine: Node = get_node("FiniteStateMachine")
+onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
+
 func _physics_process(_delta: float) -> void:
 	velocity = move_and_slide(velocity)
 	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
