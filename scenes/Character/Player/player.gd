55999a58d38147f4426a58bcffd200e906fef0b7
CJ Harries <cj@wizardsoftheweb.pro>
2024-01-12 16:37:30 -0600

    Wire up all the player components

--- scenes/Character/Player/player.gd
+++ scenes/Character/Player/player.gd
@@ -1,2 +1,21 @@
 extends Character
 class_name Player, "res://assets/sprites/hero/knight_idle_anim_f0.png"
+
+func _process(_delta: float) -> void:
+	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
+	
+	if mouse_direction.x > 0 and animated_sprite.flip_h:
+		animated_sprite.flip_h = false
+	elif mouse_direction.x < 0 and not animated_sprite.flip_h:
+		animated_sprite.flip_h = true
+
+func get_input() -> void:
+	move_direction = Vector2.ZERO
+	if Input.is_action_pressed("ui_down"):
+		move_direction += Vector2.DOWN
+	if Input.is_action_pressed("ui_left"):
+		move_direction += Vector2.LEFT
+	if Input.is_action_pressed("ui_right"):
+		move_direction += Vector2.RIGHT
+	if Input.is_action_pressed("ui_up"):
+		move_direction += Vector2.UP
